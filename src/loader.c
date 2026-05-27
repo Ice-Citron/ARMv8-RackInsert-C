3c9e0d9c8895a9d860e0a331c07f5539765c720d
Ice-Citron <shng2025@gmail.com>
2026-05-27 22:54:20 +0100

    Attempting to fix up load_program, realised that I could cross-use a macros definition of MEM_SIZE from emulate.h

--- src/loader.c
+++ src/loader.c
@@ -1,16 +1,34 @@
 #include <loader.h>
 
-int load_program(const char *path, uint8_t memory[], size_t memory_size,
-                 size_t *bytes_loaded) {
+#include <emulate.h>
+#include <stdint.h>
+
+
+int load_program(const char *path, uint8_t memory[], size_t *bytes_loaded) {
+    if (file)
+    
     FILE* file = fopen(path, "r");
     if (file == null) {
+        perror("ERROR: Failed to open file at path: %s\n", path);
         return 0;
     }
-    
+
+    // Initially, try reading MEM_SIZE bytes. If file is bigger than MEM_SIZE,
+    // then second fread() will > 0.
+    size_t bytes_read = fread(memory, sizeof(*memory), MEM_SIZE, file);
+
+    unsigned char extra;
+    if (fread(&extra, 1, 1, file) > 0) {
+        perror("ERROR: Unable to fully load program into RAM, as program size
+                > RAM size.\n");
+        fclose(file);
+        return 0;
+    }
 
     fclose(file);
+    *bytes_loaded = bytes_read;
     
-    return 0;
+    return 1;
 }
 
 uint32_t fetch_u32_le(const uint8_t memory[], uint64_t address) {
--- src/loader.h
+++ src/loader.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+
 int load_program(const char *path, uint8_t memory[], size_t memory_size,
                  size_t *bytes_loaded);
 uint32_t fetch_u32_le(const uint8_t memory[], uint64_t address);
