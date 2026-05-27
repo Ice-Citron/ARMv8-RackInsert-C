44c56c2c80babd3e62534895800b86362da81dc2
Ice-Citron <shng2025@gmail.com>
2026-05-27 22:58:50 +0100

    Finished writing load_program function, changed type to , and fixed a few bugs upon doing gcc -std=c17 -D_POSIX_SOURCE=200809L -D_DEFAULT_SOURCE -Wall -Wextra -pedantic -Wshadow -Wconversion -g -c dp_imm.c -o build/dp_imm.o.

--- src/loader.c
+++ src/loader.c
@@ -4,13 +4,11 @@
 #include <stdint.h>
 
 
-int load_program(const char *path, uint8_t memory[], size_t *bytes_loaded) {
-    if (file)
-    
-    FILE* file = fopen(path, "r");
-    if (file == null) {
-        perror("ERROR: Failed to open file at path: %s\n", path);
-        return 0;
+bool load_program(const char *path, uint8_t memory[], size_t *bytes_loaded) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "ERROR: Failed to open file at path: %s\n", path);
+        return false;
     }
 
     // Initially, try reading MEM_SIZE bytes. If file is bigger than MEM_SIZE,
@@ -19,16 +17,15 @@ int load_program(const char *path, uint8_t memory[], size_t *bytes_loaded) {
 
     unsigned char extra;
     if (fread(&extra, 1, 1, file) > 0) {
-        perror("ERROR: Unable to fully load program into RAM, as program size
-                > RAM size.\n");
+        fprintf(stderr, "ERROR: Unable to fully load program into RAM, as 
+            Program File size > RAM size.\n");
         fclose(file);
-        return 0;
+        return false;
     }
 
     fclose(file);
     *bytes_loaded = bytes_read;
-    
-    return 1;
+    return true;
 }
 
 uint32_t fetch_u32_le(const uint8_t memory[], uint64_t address) {
