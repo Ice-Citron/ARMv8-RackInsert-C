cdc82b4f299190ed03326a796769c051aa4b5340
Ice-Citron <shng2025@gmail.com>
2026-05-27 21:33:51 +0100

    Writing out the function declaration of loader.h for binary-file loader and loader.c. Currently begun implementing load_program using fread, fopen, fclose, etc.

---
+++ src/loader.c
@@ -0,0 +1,26 @@
+#include <loader.h>
+
+int load_program(const char *path, uint8_t memory[], size_t memory_size,
+                 size_t *bytes_loaded) {
+    FILE* file = fopen(path, "r");
+    if (file == null) {
+        return 0;
+    }
+    
+
+    fclose(file);
+    
+    return 0;
+}
+
+uint32_t fetch_u32_le(const uint8_t memory[], uint64_t address) {
+
+}
+
+void run_emulator(void) {
+
+}
+
+void write_final_state(FILE *out) {
+
+}
