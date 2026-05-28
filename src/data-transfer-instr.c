414761bcd06014a7d3220644e25eb78a1470fa05
Abdallah, Rayan <ra2825@ic.ac.uk>
2026-05-28 03:43:47 +0100

    working on unsigned immediate offset case of single data transfer.

--- src/data-transfer-instr.c
+++ src/data-transfer-instr.c
@@ -38,16 +38,51 @@ uint8_t sign64 (uint64_t target) {
     return bitmask_check(63, 63, target);
 }
 
-#define DATA_TRANSFER_LOAD_OP 1
+#define UNSIGNED_IMM_OFFSET 1
 #define IS_32BIT_RES 1
+#define IS_LOAD_OP 1
 
 void single_data_transfer(uint32_t instr)
 {
     clearstate(&pState);
-    uint32_t targetRegAddr = bitmask_check(0, 4, instr);
+    uint32_t RtAddr = bitmask_check(0, 4, instr);
+    uint32_t baseRegAddr = bitmask_check(5, 9, instr);
     uint32_t sizeToggle = bitmask_check(30, 30, instr);
-    if (bitmask_check(22, 22, instr) == DATA_TRANSFER_LOAD_OP) // load operation
+    if (bitmask_check(24, 24, instr) == UNSIGNED_IMM_OFFSET) // unsigned immediate offset
     {
+        uint32_t imm12 = bitmask_check(10, 21, instr);
+        uint64_t target = registers[baseRegAddr];
+        if (sizeToggle == IS_32BIT_RES) // sign flag is 1 so we're changing in 32 bit mode
+        {
+            target += (imm12 << 2); // imm12 * 4
+        }
+        else
+        {
+            target += (imm12 << 3); // imm12 * 8
+        }
+        if (bitmask_check(22, 22, instr) == IS_LOAD_OP) // load operation
+        {
+            if (sizeToggle == IS_32BIT_RES)
+            {   // set last 32 bits to 0
+                registers[RtAddr] = 0 | memory[target];
+            }
+            else
+            {
+                registers[RtAddr] = memory[target];
 
+            }
+        }
+        else // store operation
+        {
+            if (sizeToggle == IS_32BIT_RES)
+            {   // preserve last 32 bits
+                memory[target] = bitmask_check(63, 32, memory[target]) << 32;
+                memory[target] |= registers[RtAddr];
+            }
+            else
+            {
+                memory[target] = registers[RtAddr];
+            }
+        }
     }
 }
