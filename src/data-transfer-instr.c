d5083a7ae8cf3f9c61c5b113438a9b1422c004dd
Abdallah, Rayan <ra2825@ic.ac.uk>
2026-05-28 11:13:46 +0100

    rebased with emulator and fixed headers.

--- src/data-transfer-instr.c
+++ src/data-transfer-instr.c
@@ -1,43 +1,4 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdio.h>
-
-#define MEM_SIZE (1 << 21)
-#define REGS 31
-
-uint8_t memory[MEM_SIZE];
-uint64_t registers[REGS];
-uint64_t pc;
-
-const uint64_t zeroRegister = 0;
-
-typedef struct {
-    bool n;
-    bool z;
-    bool c;
-    bool v;
-} state;
-
-state pState = {false, false, false, false};
-
-uint32_t bitmask (uint8_t start, uint8_t end) {
-    //generates the bitmask which starts at start and ends at end
-    return ((1 << (start - end + 1)) - 1) << end;
-}
-
-uint32_t bitmask_check (uint8_t start, uint8_t end, uint32_t target) {
-    return (bitmask(start, end) & target) >> end;
-}
-
-uint8_t sign32 (uint64_t target) {
-    return bitmask_check(31, 31, target);
-}
-
-uint8_t sign64 (uint64_t target) {
-    return bitmask_check(63, 63, target);
-}
-
+#include "dp.h"
 #define UNSIGNED_IMM_OFFSET 1
 #define IS_32BIT_RES 1
 #define IS_LOAD_OP 1
