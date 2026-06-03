3b29400fb797d089bb87edaa6ca04962d1ebfc0b
Ice-Citron <shng2025@gmail.com>
2026-06-03 13:41:15 +0100

    Code refactoring, reduced code wordiness by changing macros of _START and _END to _HI, _LO instead; and also conducted additional code cleanup by repackaging them inside smaller functions to significantly improve code readability

--- src/dp_immediate.c
+++ src/dp_immediate.c
@@ -1,20 +1,20 @@
 #include "dp_immediate.h"
 
 void dpimm(uint32_t instr) {
-    uint32_t sf_dpimm  = extract_bits(SF_DPIMM_START, SF_DPIMM_END, instr);
-    uint32_t opc_dpimm = extract_bits(OPC_DPIMM_START, OPC_DPIMM_END, instr);
-    uint32_t opi_dpimm = extract_bits(OPI_DPIMM_START, OPI_DPIMM_END, instr);
-    uint32_t rd_dpimm  = extract_bits(RD_DPIMM_START, RD_DPIMM_END, instr);
+    uint32_t sf_dpimm  = extract_bits(SF_DPIMM_HI, SF_DPIMM_LO, instr);
+    uint32_t opc_dpimm = extract_bits(OPC_DPIMM_HI, OPC_DPIMM_LO, instr);
+    uint32_t opi_dpimm = extract_bits(OPI_DPIMM_HI, OPI_DPIMM_LO, instr);
+    uint32_t rd_dpimm  = extract_bits(RD_DPIMM_HI, RD_DPIMM_LO, instr);
     uint64_t mask = mask_from_sf(sf_dpimm);
     uint64_t entry = 0;
 
     if (opi_dpimm == OPI_DPIMM_ARITH) {
-        uint32_t rn_arith    = extract_bits(RN_DPIMM_ARITH_START,
-                                            RN_DPIMM_ARITH_END, instr);
-        uint32_t shift_arith = extract_bits(SH_DPIMM_ARITH_START,
-                                            SH_DPIMM_ARITH_END, instr);
-        uint64_t operand2    = extract_bits(IMM12_DPIMM_START,
-                                            IMM12_DPIMM_END, instr);
+        uint32_t rn_arith    = extract_bits(RN_DPIMM_ARITH_HI,
+                                            RN_DPIMM_ARITH_LO, instr);
+        uint32_t shift_arith = extract_bits(SH_DPIMM_ARITH_HI,
+                                            SH_DPIMM_ARITH_LO, instr);
+        uint64_t operand2    = extract_bits(IMM12_DPIMM_HI,
+                                            IMM12_DPIMM_LO, instr);
         uint64_t rn_value    = read_dp_register(rn_arith) & mask;
         
         if (shift_arith) {
@@ -25,10 +25,10 @@ void dpimm(uint32_t instr) {
                                        sf_dpimm);
         write_dp_result(rd_dpimm, sf_dpimm, entry);
     } else if (opi_dpimm == OPI_DPIMM_WIDE_MOVE) {
-        uint32_t hw_wm = extract_bits(SH_DPIMM_WM_START, SH_DPIMM_WM_END,
+        uint32_t hw_wm = extract_bits(SH_DPIMM_WM_HI, SH_DPIMM_WM_LO,
                                       instr);
-        uint64_t imm16_wm = extract_bits(IMM16_DPIMM_WM_START,
-                                         IMM16_DPIMM_WM_END, instr);
+        uint64_t imm16_wm = extract_bits(IMM16_DPIMM_WM_HI,
+                                         IMM16_DPIMM_WM_LO, instr);
         uint32_t shift = DPIMM_WIDE_MOVE_SHIFT_FACTOR * hw_wm;
         uint64_t shifted = (imm16_wm << shift) & mask;
         switch (opc_dpimm) {
--- src/dp_register.c
+++ src/dp_register.c
@@ -40,10 +40,10 @@ static void set_logical_flags(uint64_t entry, uint32_t sf) {
 static uint64_t compute_multiply_result(uint32_t instr, uint64_t rn, 
                                         uint64_t rm, uint64_t mask) {
     uint64_t entry = 0;
-    uint32_t ra_index = extract_bits(RA_DPREG_MULT_START,
-                                     RA_DPREG_MULT_END, instr);
-    uint32_t x_mult   = extract_bits(X_DPREG_MULT_START,
-                                     X_DPREG_MULT_END, instr);
+    uint32_t ra_index = extract_bits(RA_DPREG_MULT_HI,
+                                     RA_DPREG_MULT_LO, instr);
+    uint32_t x_mult   = extract_bits(X_DPREG_MULT_HI,
+                                     X_DPREG_MULT_LO, instr);
     uint64_t ra_mult = read_dp_register(ra_index) & mask;
     uint64_t product = (rn * rm) & mask;
 
@@ -60,7 +60,7 @@ static uint64_t compute_logical_result(uint32_t instr, uint32_t opcode,
                                        uint32_t sf) {
     uint64_t entry = 0;
     uint64_t mask = mask_from_sf(sf);
-    if (extract_bits(N_DPREG_START, N_DPREG_END, instr) == 1) {
+    if (extract_bits(N_DPREG_HI, N_DPREG_LO, instr) == 1) {
         operand2 = ~operand2 & mask;
     }
     switch (opcode) {
@@ -73,7 +73,7 @@ static uint64_t compute_logical_result(uint32_t instr, uint32_t opcode,
         case OPC_DPREG_EOR:
             entry = rn ^ operand2;
             break;
-        case OPC_DPREG_AND_BRICS:
+        case OPC_DPREG_ANDs_BRICS:
             entry = rn & operand2;
             clear_pstate_flags();
             set_logical_flags(entry, sf);
@@ -87,12 +87,12 @@ static uint64_t compute_logical_result(uint32_t instr, uint32_t opcode,
 }
 
 void dpreg(uint32_t instr) {
-    uint32_t sf_dpreg  = extract_bits(SF_DPREG_START , SF_DPREG_END , instr);
-    uint32_t opc_dpreg = extract_bits(OPC_DPREG_START, OPC_DPREG_END, instr);
-    uint32_t m_dpreg   = extract_bits(M_DPREG_START  , M_DPREG_END  , instr);
-    uint32_t rd_index  = extract_bits(RD_DPREG_START , RD_DPREG_END , instr);
-    uint32_t rn_index  = extract_bits(RN_DPREG_START , RN_DPREG_END , instr);
-    uint32_t rm_index  = extract_bits(RM_DPREG_START , RM_DPREG_END , instr);
+    uint32_t sf_dpreg  = extract_bits(SF_DPREG_HI , SF_DPREG_LO , instr);
+    uint32_t opc_dpreg = extract_bits(OPC_DPREG_HI, OPC_DPREG_LO, instr);
+    uint32_t m_dpreg   = extract_bits(M_DPREG_HI  , M_DPREG_LO  , instr);
+    uint32_t rd_index  = extract_bits(RD_DPREG_HI , RD_DPREG_LO , instr);
+    uint32_t rn_index  = extract_bits(RN_DPREG_HI , RN_DPREG_LO , instr);
+    uint32_t rm_index  = extract_bits(RM_DPREG_HI , RM_DPREG_LO , instr);
     uint64_t mask = mask_from_sf(sf_dpreg);
     uint64_t rn = read_dp_register(rn_index) & mask;
     uint64_t rm = read_dp_register(rm_index) & mask;
@@ -102,11 +102,11 @@ void dpreg(uint32_t instr) {
         entry = compute_multiply_result(instr, rn, rm, mask);
         write_dp_result(rd_index, sf_dpreg, entry);
     } else {
-        uint32_t opr0 = extract_bits(OPR0_DPREG_START, OPR0_DPREG_END, instr);
-        uint32_t shift_type = extract_bits(SHIFT_DPREG_START,
-                                           SHIFT_DPREG_END, instr);
-        uint32_t shift_dist = extract_bits(SHIFT_DIST_DPREG_START,
-                                           SHIFT_DIST_DPREG_END, instr);
+        uint32_t opr0 = extract_bits(OPR0_DPREG_HI, OPR0_DPREG_LO, instr);
+        uint32_t shift_type = extract_bits(SHIFT_DPREG_HI,
+                                           SHIFT_DPREG_LO, instr);
+        uint32_t shift_dist = extract_bits(SHIFT_DIST_DPREG_HI,
+                                           SHIFT_DIST_DPREG_LO, instr);
         uint64_t operand2 = shift_operand(rm, shift_type, shift_dist, sf_dpreg);
         if (shift_type == SHIFT_TYPE_ROR && opr0 == OPR0_DPREG_ARITH) {
             fprintf(stderr, "ERROR: Invalid rotate right with arithmetic op\n");
