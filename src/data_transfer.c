3d591a7587477a820a6ffc1bbffb6626b6677472
Ice-Citron <shng2025@gmail.com>
2026-06-03 19:12:47 +0100

    Finalised both dp_register.c and dp_register.h for which I'm now happy with them, now working hard to clean up data_transfer.c.

--- src/data_transfer.c
+++ src/data_transfer.c
@@ -1,79 +1,81 @@
 #include "data_transfer.h"
 
-void load_operation(const uint32_t rtAddr, const uint64_t target, const int n) {
-    registers[rtAddr] = 0;
-    for (int i = 0; i < n; i++) // n = 8 bytes for 64 bits and n = 4 for 32 bits
-    {
-        registers[rtAddr] |= ((uint64_t) memory[target + (uint64_t)i] 
-                                << (i * 8)); // plug in 8 bits at a time
+static void load_operation(const uint32_t rt_index, const uint64_t target, 
+                                  const int n) {
+    registers[rt_index] = 0;
+    for (int i = 0; i < n; i++) {
+        uint64_t memAddr = target + (uint64_t)i;    
+        registers[rt_index] |= ((uint64_t)memory[memAddr] << (i * 8)); // plug in 8 bits at a time
     }
 }
 
-void store_operation(const uint32_t rtAddr, const uint64_t target, const int n) {
-    for (int i = 0; i < n; i++) // n = 8 bytes for 64 bits and n = 4 for 32 bits
-    {
-        // code to copy value of bits in registers into the memory locations
-        memory[target + (uint64_t)i] = (uint8_t)(registers[rtAddr] >> (i * 8)) & 0xff; // will implement later in a helper
+static void store_operation(const uint32_t rt_index, const uint64_t target, 
+                                   const int n) {
+    for (int i = 0; i < n; i++) { // n = 8 bytes for 64 bits; n = 4 for 32 bits
+        uint64_t memAddr = target + (uint64_t)i;
+        memory[memAddr] = (uint8_t)(registers[rt_index] >> (i * 8)) & 0xff; 
     }
 }
-void perform_load_or_store(const uint32_t rtAddr, const uint64_t target, 
-                           const int n, const uint32_t operation) {
-    if (operation == IS_LOAD_OP) {
-        load_operation(rtAddr, target, n);
+
+static void perform_load_or_store(const uint32_t rt_index, 
+                                         const uint64_t target, const int n, 
+                                         const uint32_t operation) {
+    if (operation == DT_LOAD_OP) {
+        load_operation(rt_index, target, n);
     }
     else {
-        store_operation(rtAddr, target, n);
+        store_operation(rt_index, target, n);
     }
 }
 
 void single_data_transfer(const uint32_t instr) {
-    const uint32_t rtAddr = extract_bits(4, 0, instr);
-    const uint32_t sizeToggle = extract_bits(30, 30, instr);
-    int numOfBytes = 8;
-    if (sizeToggle == IS_32BIT_RES) {// sf is 1 so we're changing in 32 bit mode
-        numOfBytes = 4;
+    const uint32_t rt_index = extract_bits(DT_RT_HI, DT_RT_LO, instr);
+    const uint32_t size     = extract_bits(DT_SIZE_HI, DT_SIZE_LO, instr);
+    int num_bytes = 8;
+    if (size == DT_32BIT_MODE) {
+        num_bytes = 4;
     }
-    if (extract_bits(31, 31, instr) == IS_SINGLE_DATA_TRANSFER) {
-        const uint32_t xnAddr = extract_bits(9, 5, instr);
-        const uint32_t operation = extract_bits(22, 22, instr) ;
-        uint64_t target = registers[xnAddr];
-        if (extract_bits(24, 24, instr) == UNSIGNED_IMM_OFFSET) // unsigned immediate offset
-        {
-            const uint32_t imm12 = extract_bits(21, 10, instr);
-            if (sizeToggle == IS_32BIT_RES) {
-                target += (imm12 << 2); // imm12 * 4
+    if (extract_bits(DT_SINGLE_HI, DT_SINGLE_LO, instr) == DT_SINGLE_MODE) {
+        const uint32_t xn_index = extract_bits(DT_XN_HI, DT_XN_LO, instr);
+        const uint32_t operation = extract_bits(DT_OP_HI, DT_OP_HI, instr) ;
+        uint64_t target = registers[xn_index];
+        if (extract_bits(DT_UINT_HI, DT_UINT_LO, instr) == DT_UINT_IMM_OFFSET) {
+            const uint32_t imm12 = extract_bits(DT_IMM12_HI, DT_IMM12_LO, 
+                                                instr);
+            if (size == DT_32BIT_MODE) {
+                target += (imm12 << 2);
             } else {
-                target += (imm12 << 3); // imm12 * 8
+                target += (imm12 << 3);
             }
-            perform_load_or_store(rtAddr, target, numOfBytes, operation);
+            perform_load_or_store(rt_index, target, num_bytes, operation);
         }
-        else if (extract_bits(10, 10, instr) == PRE_POST_INDEXED)
-        {
+        else if (extract_bits(DT_PP_INDEX_HI, DT_PP_INDEX_LO, instr) 
+                 == DT_PRE_POST_INDEX) {
             const long long simm9 = get_signed_value_from_bits(20, 12, instr);
-            if (extract_bits(11, 11, instr) == PRE_INDEXED)
+            if (extract_bits(11, 11, instr) == DT_PRE_INDEXED)
             {
                 target = (uint64_t)((long long)target + simm9);
-                registers[xnAddr] = (uint64_t)target;
-                perform_load_or_store(rtAddr, target, numOfBytes, operation);
+                registers[xn_index] = (uint64_t)target;
+                perform_load_or_store(rt_index, target, num_bytes, operation);
             }
             else // POST_INDEXED
             {
-                perform_load_or_store(rtAddr, target, numOfBytes, operation);
+                perform_load_or_store(rt_index, target, num_bytes, operation);
                 target = (uint64_t)((long long)target + simm9);
-                registers[xnAddr] = (uint64_t)target;
+                registers[xn_index] = (uint64_t)target;
             }
         }
-        else if (extract_bits(15, 10, instr) == IS_REG_OFFSET_MODE) // register offset
+        else if (extract_bits(15, 10, instr) == DT_REG_OFFSET_MODE) // register offset
         {
-            const uint32_t xmAddr = extract_bits(20, 16, instr);
-            target += registers[xmAddr]; // might need to have a check that xm is <=30
-            perform_load_or_store(rtAddr, target, numOfBytes, operation);
+            const uint32_t xm_index = extract_bits(20, 16, instr);
+            target += registers[xm_index]; // might need to have a check that xm is <=30
+            perform_load_or_store(rt_index, target, num_bytes, operation);
         }
     }
     else // IS_LOAD_LITERAL
     {
         long long offset = get_signed_value_from_bits(23, 5, instr) << 2;
         uint64_t target = (uint64_t)((long long)pc + offset);
-        load_operation(rtAddr, target, numOfBytes);
+        load_operation(rt_index, target, num_bytes);
     }
 }
--- src/data_transfer.h
+++ src/data_transfer.h
@@ -4,18 +4,32 @@
 #include "emulate.h"
 #include "bit_manipulation.h"
 
-#define UNSIGNED_IMM_OFFSET 1
-#define PRE_POST_INDEXED 1
-#define PRE_INDEXED 1
-#define IS_32BIT_RES 0
-#define IS_LOAD_OP 1
-#define IS_REG_OFFSET_MODE 26
-#define IS_SINGLE_DATA_TRANSFER 1
+#define DT_UINT_IMM_OFFSET 1
+#define DT_PRE_POST_INDEX 1
+#define DT_PRE_INDEXED 1
+#define DT_32BIT_MODE 0
+#define DT_LOAD_OP 1
+#define DT_SINGLE_MODE 1
+#define DT_REG_OFFSET_MODE 26
 
-void load_operation(const uint32_t rtAddr, const uint64_t target, const int n);
-void store_operation(const uint32_t rtAddr, const uint64_t target, const int n);
-void perform_load_or_store(const uint32_t rtAddr, const uint64_t target, 
-                           const int n, const uint32_t operation);
+#define DT_RT_HI 4
+#define DT_RT_LO 0
+#define DT_SIZE_HI 30
+#define DT_SIZE_LO 30
+#define DT_XN_HI 9
+#define DT_XN_LO 5
+#define DT_OP_HI 22
+#define DT_OP_HI 22
+#define DT_SINGLE_HI 31
+#define DT_SINGLE_LO 31
+#define DT_UINT_HI 24
+#define DT_UINT_LO 24
+#define DT_IMM12_HI 21
+#define DT_IMM12_LO 10
+#define DT_PP_INDEX_HI 10
+#define DT_PP_INDEX_LO 10
+
+
 void single_data_transfer(const uint32_t instr);
 
 #endif
