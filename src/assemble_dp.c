eff77991f7904bffa48a69915300eff6adc9167e
rahulbabu <rb1725@ic.ac.uk>
2026-06-04 01:36:51 +0100

    continued instruction design for data processing, now including full coverage for all possible mnemonics. Need to design encodings for different types next.

--- src/assemble_dp.c
+++ src/assemble_dp.c
@@ -2,9 +2,24 @@
 
 int assemble_dp(string mnemonic, char *operands[]) {
     if (mnemonic == "movn" || mnemonic == "movz" || mnemonic == "movk") {
-        wide_move(mnemonic, *operands)
+        return assemble_wide_move(mnemonic, *operands)
     } 
     else if (mnemonic == "madd" || mnemonic == "msub" || mnemonic == "mul" || mnemonic == "mneg") {
-        multiply(mnemonic, *operands)
+        return assemble_multiply(mnemonic, *operands)
     }
+    else if (mnemonic == "add" || mnemonic == "adds" || mnemonic == "sub"
+     || mnemonic == "subs" || mnemonic == "cmp" || mnemonic == "cmn" || mnemonic == "neg" || mnemonic == "negs") {
+        arithmetic(mnemonic, *operands)
+        if (operands[2][0] == "#") {
+            return assemble_dp_imm(mnemonic, *operands[])
+        }
+        else {
+            return assemble_dp_reg(mnemonic, *operands[])
+        }
+    }
+    else if (mnemonic == "and" || mnemonic == "bic" || mnemonic == "orr" || mnemonic == "orn" || mnemonic == "eor" 
+        || mnemonic == "eon" || mnemonic == "ands" || mnemonic == "bics" || mnemonic == "tst" || mnemonic == "mov" || mnemonic == "mvn") {
+            return assemble_logical_register()
+        }
+
 }
