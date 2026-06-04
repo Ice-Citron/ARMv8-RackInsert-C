295e7e677c56e955ae644995186a5b6b86cb5195
Abdallah, Rayan <ra2825@ic.ac.uk>
2026-06-04 01:53:48 +0100

    helper functions for identifying whether we have a label to add to the symbol table.

--- src/first_pass_helpers.c
+++ src/first_pass_helpers.c
@@ -1,5 +1,38 @@
-//
-// Created by Rayan Abdallah on 04/06/2026.
-//
-
 #include "first_pass_helpers.h"
+
+bool check_in_alias(const char *str)
+{
+    for (int i=0; i<ALIASES_SIZE; i++)
+    {
+        if (strcmp(str, aliases[i]) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool check_in_conds(const char *str)
+{
+    if (str[0] != 'b')
+    {
+        return false;
+    }
+    if (strcmp(str, "b:") == 0 || strcmp(str, "br:") == 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool check_in_instrs(const char *str)
+{
+    for (int i=0; i<INSTRS_SIZE; i++)
+    {
+        if (strcmp(str, instrs[i]) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
--- src/first_pass_helpers.h
+++ src/first_pass_helpers.h
@@ -1,8 +1,13 @@
-//
-// Created by Rayan Abdallah on 04/06/2026.
-//
+#include <stdbool.h>
+#include <string.h>
+#define CONDS_SIZE 7
+#define ALIASES_SIZE 8
+#define INSTRS_SIZE 19
 
-#ifndef SRC_FIRST_PASS_HELPERS_H
-#define SRC_FIRST_PASS_HELPERS_H
-
-#endif //SRC_FIRST_PASS_HELPERS_H
+// temporary solution for identifying branches
+static char *conds[CONDS_SIZE] = {"eq", "ne", "ge", "lt", "gt", "le", "al"};
+static char *aliases[ALIASES_SIZE] = {"cmp", "cmn", "neg(s)", "tst", "mvn",
+    "mov", "mul", "mneg"};
+static char *instrs[INSTRS_SIZE] = {"add(s)", "sub(s)", "and(s)", "bic(s)",
+    "eor", "eon", "orr", "orn", "movn", "movk", "movz", "madd", "msub", "b",
+    "br", "b.cond", "ldr", "str", "and"};
