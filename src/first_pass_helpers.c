28593842e0d55d3f33648b2dcde7ec71f6506e54
Abdallah, Rayan <ra2825@ic.ac.uk>
2026-06-04 02:00:02 +0100

    reduced repeated code.

--- src/first_pass_helpers.c
+++ src/first_pass_helpers.c
@@ -1,15 +1,21 @@
 #include "first_pass_helpers.h"
 
-bool check_in_alias(const char *str)
+bool check_in_string_array(const char *target, const char *arr[],
+    const int size)
 {
-    for (int i=0; i<ALIASES_SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
-        if (strcmp(str, aliases[i]) == 0)
+        if (strcmp(target, arr[i]) == 0)
         {
             return true;
         }
     }
     return false;
+}
+
+bool check_in_alias(const char *str)
+{
+    return check_in_string_array(str, aliases, ALIASES_SIZE);
 }
 
 bool check_in_conds(const char *str)
@@ -27,12 +33,5 @@ bool check_in_conds(const char *str)
 
 bool check_in_instrs(const char *str)
 {
-    for (int i=0; i<INSTRS_SIZE; i++)
-    {
-        if (strcmp(str, instrs[i]) == 0)
-        {
-            return true;
-        }
-    }
-    return false;
+    return check_in_string_array(str, instrs, INSTRS_SIZE);
 }
