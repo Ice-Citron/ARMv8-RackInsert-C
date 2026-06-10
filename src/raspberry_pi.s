4abe145c403d2db9aef2f56c99336c12de99041a
Ryan Wong WJ <ryan.wong25@imperial.ac.uk>
2026-06-10 13:44:28 +0100

    first draft of on-off loop

--- src/raspberry_pi.s
+++ src/raspberry_pi.s
@@ -1,4 +1,31 @@
-//setup
+//choosing io
+ldr w0 io_0to9
+movn w0, pin2_output, lsl pin2_shift
+str w0 io_0to9
+//loads the register setting address into the w0
+
+//main loop
+blink:
+    //setting on
+    ldr w0 set0
+    orr w0 pin2_on
+    str w0 set0
+    //busy-waiting
+    ldr w1 timing
+    wait1:
+        subs w1, w1, #1
+        b.ne wait1
+    //setting off 
+    orn w0 pin2_on
+    str w0 set0
+    //busy-waiting
+    ldr w1 timing
+    wait2:
+        subs w1, w1, #1
+        b.ne wait2
+    blink
+
+//constant pool 
 //using GPIO Pin 2 to control the LED
 io_0to9: .int 0x3f200000
 set_0: .int 0x3f20001c
@@ -10,25 +37,3 @@ pin2_on: .int 0x4
 timing: .int 0x00C00000
 //IIRC we are using a raspberry pi 3 which runs at 1.2 GHZ
 //we will use loop cycles to make the rpi3 wait for 0.1s between each on or off switch 
-
-//choosing io
-ldr w0 io_0to9
-movn w0, pin2_output, lsl pin2_shift
-str w0 io_0to9
-//loads the register setting address into the w0
-
-    
-
-blink:
-    //setting on
-    ldr w0 set0
-    orr w0 pin2_on
-    str w0 set0
-    //busy-waiting
-    ldr w1 timing
-
-
-    //setting off 
-    ldr w0 set0
-    orr w0 pin2_on
-    str w0 set0
