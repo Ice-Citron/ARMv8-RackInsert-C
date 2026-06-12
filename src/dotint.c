8b22b65d2953f8eb610df93cf24c1d7637e99d6b
Ryan Wong WJ <ryan.wong25@imperial.ac.uk>
2026-06-12 16:54:58 +0100

    syncing to function signature in assemble_dp

--- src/dotint.c
+++ src/dotint.c
@@ -1,16 +1,20 @@
 #include "dotint.h"
 //pre: we get a single number, either in binary or hexadecimal 
 //pre: assumed that the line is known to be .int
-uint32_t int_directive (char* inputstr) {
+uint32_t int_directive (char* mnemonic, char *operands[], size_t operand_count) {
+    if (strcmp(*mnemonic, ".int") != 0 || operand_count != 1) {
+        fprintf(stderr, "Invalid instruction parameters");
+        exit(1);
+    }
     uint8_t outputbytes[OUTPUT_BYTES_PER_LINE];
     uint32_t inputint;
     char* endptr;
-    if (strncmp(inputstr, HEX_TYPE_SIGNATURE, LEN_HEX_TYPE_SIGNATURE)) {
+    if (strncmp(operands[0], HEX_TYPE_SIGNATURE, LEN_HEX_TYPE_SIGNATURE)) {
         //shows that it is hexadecimal
-        inputint = strtol(inputstr, endptr, HEX_BASE);
+        inputint = strtol(operands[0], endptr, HEX_BASE);
     } else {
         //decimal support
-        inputint = strtol(inputstr + LEN_HEX_TYPE_SIGNATURE, endptr, HEX_BASE);
+        inputint = strtol(operands[0] + LEN_HEX_TYPE_SIGNATURE, endptr, HEX_BASE);
     }
     return inputint;
 }
