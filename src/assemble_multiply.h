2c3d81cfc79618dfd390485739822350221b034b
rahulbabu <rb1725@ic.ac.uk>
2026-06-13 01:13:06 +0100

    Refactor complete for assemble_dp

---
+++ src/assemble_multiply.h
@@ -0,0 +1,9 @@
+#ifndef ASSEMBLE_MULTIPLY_H
+#define ASSEMBLE_MULTIPLY_H
+
+#include <stdlib.h>
+#include <stddef.h>
+
+uint32_t assemble_multiply(string mnemonic, char *operands[], size_t operand_count);
+
+#endif
