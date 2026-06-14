c2430d9076f5cd7862a14d1a13682f6ef2f7bf3f
Abdallah, Rayan <ra2825@ic.ac.uk>
2026-06-14 16:33:02 +0100

    implemented load literal subtask.

--- src/ass_single_data_transfer.c
+++ src/ass_single_data_transfer.c
@@ -1,8 +1,6 @@
-//
-// Created by Rayan Abdallah on 14/06/2026.
-//
-
 #include "ass_single_data_transfer.h"
+#include "symbol_table.h"
+#include "assemble.h"
 
 uint32_t ass_single_data_transfer(char* mnemonic, char *operands[],
                     size_t operand_count, uint32_t pc)
@@ -13,24 +11,40 @@ uint32_t ass_single_data_transfer(char* mnemonic, char *operands[],
     }
     uint32_t res = 0;
     uint8_t reg_t = atoi(&operands[0][1]);
-    bool in_64_bits = false;
+    int num_bytes = 4;
     if (operands[0][0] == 'X')
     {
         res |= 1 << SF_BIT;
-        in_64_bits = true;
+        num_bytes = 8;
     }
     if (operands[1][0] != '[') // LOAD LITERAL
     {
         res |= LOAD_LITERAL_BITS << LOAD_LITERAL_BITS_POS;
+        int target_addr = 0;
+        if (operands[1][0] == '#') // literal
+        {
+            int literal = atoi(&operands[1][1]);
+            mem_addr += (literal >> 2);
+            load_operation(reg_t, mem_addr, num_bytes);
+        }
+        else // label
+        {
+            target_addr = find_address_from_sym_table(operands[1]);
+        }
+        int64_t offset = offset = (int64_t)target_addr - (int64_t)pc;
+        int64_t simm19 = (offset >> 2) & SIM_19_BIT_MASK;
+        res |= simm19 << SIM_19_POS;
+        res |= (reg_t & NUM_OF_REGISTERS);
+        return res;
     }
-    if (strcmp(mnemonic, "ldr"))
+    if (strcmp(mnemonic, "ldr") == 0)
     {
         res |= 1 << MOST_SIG_BIT;
         res |= 1 << L_BIT;
     }
-    else if (strcmp(mnemonic, "str"))
+    else if (strcmp(mnemonic, "str") == 0)
     {
-        uint32_t res = 1 << MOST_SIG_BIT;
+        res = 1 << MOST_SIG_BIT;
     }
     else
     {
--- src/symbol_table.h
+++ src/symbol_table.h
@@ -4,3 +4,9 @@
 
 #define MAX_SYMBOL_LEN 64
 #define MAX_SYMBOL_COUNT (1 << 21)
+
+void init_symbol_table();
+
+uint32_t find_address_from_sym_table(const char *name);
+
+void add_to_symbol_table(const char *name, const uint32_t address);
