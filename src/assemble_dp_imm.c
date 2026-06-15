13da3307b3d5c08903a29ea8bc576aadc4975208
rahulbabu <rb1725@ic.ac.uk>
2026-06-15 04:05:34 +0100

    Completed immediate rn and rd parsing

--- src/assemble_dp_imm.c
+++ src/assemble_dp_imm.c
@@ -1,8 +1,9 @@
 #include <assemble_dp_imm.h>
 #include <assemble_dp.h>
 
+
 uint32_t assemble_dp_imm(string mnemonic, char *operands[], size_t operand_count) {
-    uint32_t sf = 0u;
+    uint32_t sf = 0u << SF_SHIFT;
     uint32_t opc = 0u;
     uint32_t rd = 0u;
     uint32_t rn = 0u;
@@ -36,5 +37,44 @@ uint32_t assemble_dp_imm(string mnemonic, char *operands[], size_t operand_count
         opc = OPC_SUB;
     } else if (strcmp(mnemonic, "subs") == 0 || strcmp(mnemonic, "cmp") == 0) {
         opc = OPC_SUBS;
+    }
+
+    if (operands[0][0] == 'x') {
+        sf = 1u << SF_SHIFT;
+    } else if (operands[0][0] != 'w') {
+        fprintf(stderr, "ERROR: invalid register %s\n", operands[0]);
+        exit(1);
+    }
+
+    char *end = NULL;
+
+    if (rd != ZERO_REGISTER_NUMBER) {
+        rd = (uint32_t) strtoul(operands[0] + 1, &end, 10);
+        if (*end != '\0' || rd > ZERO_REGISTER_NUMBER) {
+            fprintf(stderr, "ERROR: Invalid destination register %s\n", operands[0]);
+            exit(1);
+        }
+    }
+
+    if (rn != ZERO_REGISTER_NUMBER) {
+        end = NULL;
+        rn = (uint32_t) strtoul(operands[rn_index] + 1, &end, 10);
+        if (*end != '\0' || rn > ZERO_REGISTER_NUMBER) {
+            fprintf(stderr, "ERROR: Invalid source register %s\n", operands[rn_index])
+            exit(1);
+        }
+    }
+
+    if (operands[imm_index][0] != '#') {
+        fprtinf(stderr, "ERROR: Invalid immediate addressing format, must begin with # %s\n", operands[imm_index])
+    }
+
+    end = NULL;
+    unsigned long parsed_imm = strtoul(operands[imm_index] + 1, &end, 10);
+
+    if (*end != '\0')
+
+
+
 
 }
