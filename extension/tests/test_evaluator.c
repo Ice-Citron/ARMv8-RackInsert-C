dd354b8e54a3b702aadb83b3abf9ea0f10322e70
Ice-Citron <shng2025@gmail.com>
2026-06-15 15:21:39 +0100

    Filling out test cases for eval_compute_geometry within test_evaluator.c. To guarantee code correctness.

--- extension/tests/test_evaluator.c
+++ extension/tests/test_evaluator.c
@@ -7,3 +7,32 @@
 static void assert_close(double actual, double expected, double tol) {
     assert(fabs(actual - expected) <= tol);
 }
+
+static void test_geometry_halfway_inserted(void) {
+    EvalGeometry geom = {
+        .plug_tip      = {0.0, 0.0, 0.05},
+        .socket_mouth  = {0.0, 0.0, 0.0},
+        .socket_bottom = {0.0, 0.0, 0.10},
+    };
+
+    TrialScore score = {0};
+    eval_compute_geometry(&geom, &score);
+
+    assert_close(score.axial_depth, 0.05, 1e-9);
+    assert_close(score.lateral_error, 0.0, 1e-9);
+    assert_close(score.plug_port_distance, 0.05, 1e-9);
+}
+
+static void test_geometry_lateral_error(void) {
+    Eval_Geometry geom = {
+        .plug_tip      = {0.03, 0.0, 0.05},
+        .socket_mouth  = {0.0, 0.0, 0.0},
+        .socket_bottom = {0.0, 0.0, 0.10},
+    };
+
+    TrialScore score = {0};
+    eval_compute_geometry(&geom, &score);
+
+    assert_close(score.axial_depth, 0.05, 1e-9);
+    assert_close(score.lateral_error, 0.03, 1e-9);
+}
