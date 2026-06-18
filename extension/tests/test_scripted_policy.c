05239203ad1b32446cafa56555f400c9d2cd6d6f
Ice-Citron <shng2025@gmail.com>
2026-06-18 19:20:41 +0100

    Syntactical error bug fixes applied, as make clean test failed to run due to syntactical errors as well as missing semi-colons.

--- extension/tests/test_scripted_policy.c
+++ extension/tests/test_scripted_policy.c
@@ -55,10 +55,43 @@ int main(void) {
         // (lower path length is better, more efficient, don't want arm 
         // zig-zagging unneccesarily).
         path_length += vec3_distance(previous_tip, current_tip);
+
+        previous_tip[0] = current_tip[0];
+        previous_tip[1] = current_tip[1];
+        previous_tip[2] = current_tip[2];
     }
 
-    
+    read_geometry(&sim, &final_geom);
+    eval_compute_geometry(&final_geom, &final_score);
 
-    print_trial_summary();
+    final_score.initial_plug_port_distance = initial_score.plug_port_distance;
+    final_score.path_length = path_length;
+    final_score.duration = sim.data->time;
+    final_score.average_jerk = 0.0;
+    final_score.retries = policy.retries;
+
+    eval_score_trial(&config, socket_depth, &final_score);
+
+    const char *title = "SCRIPTED POLICY ROLLOUT SMOKE TEST"
+    double plug_motion = vec3_distance(initial_geom.plug_tip, 
+                                       final_geom.plug_tip);
+    print_trial_summary(title, &initial_score, &final_score, plug_motion, 
+                        sim.data->time);
+    
+    int policy_finished = scripted_policy_is_done(&policy);
+    sim_free(&sim);
+
+    if (!policy_finished) {
+        fprintf(stderr, 
+                "ERROR: [SMOKE TEST FAILED]: scripted_policy timed out\n");
+        return EXIT_FAILURE;
+    }
+    if (plug_motion < MIN_PLUG_MOTION_M) {
+        fprintf(stderr, 
+                "ERROR: [SMOKE TEST FAILED]: TCP barely moved\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("[SMOKE TEST SUCCESS]: scripted_policy completed smoke tests.\n");
     return EXIT_SUCCESS;
 }
