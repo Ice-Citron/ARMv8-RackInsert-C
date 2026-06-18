cdd29bffa8cb558430581a419c6409fb2e169892
Ice-Citron <shng2025@gmail.com>
2026-06-18 23:26:44 +0100

    Currently creating benchmark.ch module. THis is a simple module as it's literally just a packaged version of the smoke test for test_scripted_policy module.

---
+++ extension/include/benchmark.h
@@ -0,0 +1,25 @@
+#ifndef EXTENSION_BENCHMARK_H
+#define EXTENSION_BENCHMARK_H
+
+#include "sim.h"
+#include "evaluator.h"
+
+typedef struct {
+    TrialScore initial_score;
+    TrialScore final_score;
+
+    double initial_plug_tip[3];
+    double final_plug_tip[3];
+
+    double socket_depth;
+    double plug_motion;
+    double duration;
+    double path_length;
+
+    int policy_finished;
+} BenchmarkResult;
+
+void benchmark_run_scripted_policy_trial(Sim *sim, double max_seconds,
+                                         BenchmarkResult *result);
+
+#endif
--- extension/src/vec.c
+++ extension/src/vec.c
@@ -42,3 +42,9 @@ void vec3_normalize(const double v[3], double out[3]) {
         out[i] = v[i] / magnitude;
     }
 }
+
+void copy_vec3(const double src[3], const double dst[3]) {
+    for (int i = 0; i < 3; i++) {
+        dst[i] = src[i];
+    }
+}
