489a5a586ad1a836610cb8bd98ef7f2eef2bc88e
Ryan Wong WJ <ryan.wong25@imperial.ac.uk>
2026-06-18 14:32:05 +0100

    bugfix arm code

--- src/raspberry_pi.s
+++ src/raspberry_pi.s
@@ -1,29 +1,29 @@
 //choosing io (we choose pin 2 because it is small)
-ldr w0 io_0to9
+ldr w0, io_0to9
 movn w0, pin2_output, lsl pin2_shift
-str w0 io_0to9
+str w0, io_0to9
 //loads the register setting address into the w0
 //main loop
 blink:
     //setting on
-    ldr w0 set_0
-    orr w0 pin2_on
-    str w0 set_0
+    ldr w0, set_0
+    orr w0, pin2_on
+    str w0, [w0]
     //busy-waiting
-    ldr w1 timing
+    ldr w1, timing
     wait1:
         subs w1, w1, #1
         b.ne wait1
     //setting off
-    ldr w0 clr_0
-    orr w0 pin2_on
-    str w0 clr_0
+    ldr w0, clr_0
+    orr w0, pin2_on
+    str w0, [w0]
     //busy-waiting
-    ldr w1 timing
+    ldr w1, timing
     wait2:
         subs w1, w1, #1
         b.ne wait2
-    blink
+    b blink
 //----------------------------------------------------------
 //constant pool 
 //using GPIO Pin 2 to control the LED
@@ -31,9 +31,9 @@ io_0to9: .int 0x3f200000
 set_0: .int 0x3f20001c
 clr_0: .int 0x3f200028
 pin2_shift: .int 0x6
-pin2_output: .int 0x6
+pin2_output: .int 0x1
 //makes pin2 an input by using OR bitmask
 pin2_on: .int 0x4
-timing: .int 0x00C00000
+timing: .int 0x03938700
 //IIRC we are using a raspberry pi 3 which runs at 1.2 GHZ
 //we will use loop cycles to make the rpi3 wait for 0.1s between each on or off switch
