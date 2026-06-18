db792976d965498fba1fe017d381c712c5595b31
Ice-Citron <shng2025@gmail.com>
2026-06-19 00:16:20 +0100

    Just written CLI parser inside main function, to account for cases where users define --trials and --scene.

--- extension/src/main.c
+++ extension/src/main.c
@@ -18,7 +18,27 @@ int main(int argc, char **argv) {
     const char *scene_path = DEFAULT_SCENE_PATH;
     int trials = 1;
 
-    
+    // Dynamic CLI parser which allows user to set --scene and --trials
+    //    - Starts from i=1 because i=0 is char *program_name
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--scene")) {
+            if (i + 1 >= argc) {
+                print_usage_guide(argv[0]);
+                return EXIT_FAILURE;
+            }
+            scene_path = argv[++i];
+        } else if (strcmp(argv[i], "--trials") == 0) {
+            int parse_success = parse_int_arg(argv[i + 1], &trials);
+            if (i + 1 >= argc || !parse_success) {
+                print_usage_guide(argv[0]);   // argv[0] == char *program_name
+                return EXIT_FAILURE;
+            }
+            i++;
+        } else { // If doesn't parse to `--scene` or `--trials`
+            print_usage_guide(argv[0]);
+            return EXIT_FAILURE;
+        }
+    }
 
     sim_free(&sim);
     return EXIT_SUCCESS;
